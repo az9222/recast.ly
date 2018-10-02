76e515e9e18c17197eeb3351dafaae343025c2e7
az9222 <angelazhou922@gmail.com>
2018-10-01 17:08:40 -0700

    Video lists show

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,21 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videos = this.props.videos;
+  }
+
+  render() {
+    return (
+      <div className="video-list">
+        {this.videos.map(video =>
+          <VideoListEntry video={video} />
+        )}
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,27 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+import VideoList from './VideoList.js';
+
+class VideoListEntry extends React.Component {
+  constructor (props) {
+    super(props);
+    this.videoImg = this.props.video.snippet.thumbnails.default.url;
+    this.videoTitle = this.props.video.snippet.title;
+    this.videoDescription = this.props.video.snippet.description;
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.videoImg} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{this.videoTitle}</div>
+          <div className="video-list-entry-detail">{this.videoDescription}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
